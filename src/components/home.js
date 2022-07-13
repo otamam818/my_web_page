f8e69c805b0817a764e6ed766a4dbbf005cdbda8
otamam818 <otamam818@gmail.com>
2022-07-14 01:10:20 +1000

    Added pictures and polishes

--- src/components/home.js
+++ src/components/home.js
@@ -1,12 +1,34 @@
 import React from 'react';
-import "../styles/homepage.css"
+import "../styles/homepage.css";
 
 function Home(props) {
+    let skills = Object.entries(require('../assets/skills.json'));
+    let skillElement = skills.map((pairs, _) => {
+        let key = pairs[0];
+        let metadata = pairs[1];
+
+        let myImg = require(`../pictures/${metadata.ImageName}`);
+        let subSkills = metadata.SubSkills.map((subSkill, index) => {
+            return <li key={index}>{subSkill}</li>
+        });
+
+        return (
+            <li key={key}>
+              <img src={myImg} alt="skill"/>
+              <span className="key">{key}</span>
+              <ul>
+                {subSkills}
+              </ul>
+            </li>
+        )
+    });
+
     return (
         <div className="homepage">
             <div className="picture-container">
                 <img src={require("../pictures/mypic.png")} alt="Profile"/>
             </div>
+            <ol className="skill-list">{skillElement}</ol>
         </div>
     )
 }
