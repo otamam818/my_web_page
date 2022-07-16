7097060eea8c89420a7162fd513db37e50e04ad4
otamam818 <otamam818@gmail.com>
2022-07-16 12:17:47 +1000

    Implemented rotation for showcasing different skills

--- src/components/home.js
+++ src/components/home.js
@@ -1,19 +1,35 @@
 import React from 'react';
 import "../styles/homepage.css";
 
+const IMG_HEIGHT = '35vh';
+
 function Home(props) {
     let skills = Object.entries(require('../assets/skills.json'));
-    let skillElement = skills.map((pairs, _) => {
+    let each_angle = (2*Math.PI) / skills.length;
+    let skillElement = skills.map((pairs, num_element) => {
         let key = pairs[0];
         let metadata = pairs[1];
 
+        // Place the data inside the component
         let myImg = require(`../pictures/${metadata.ImageName}`);
         let subSkills = metadata.SubSkills.map((subSkill, index) => {
-            return <li key={index}>{subSkill}</li>
+            return <li key={index} className="subSkill">{subSkill}</li>
         });
 
+        // Get the position of the component
+        let offsetX = 5.5;
+        let offsetY = 2.0;
+        let rotated_angle = (each_angle * num_element) - (Math.PI * (3/4))
+        let hyp  = parseFloat(IMG_HEIGHT);
+        let top  = (Math.sin(rotated_angle) * hyp - offsetY).toFixed(1);
+        let left = (Math.cos(rotated_angle) * hyp - offsetX).toFixed(1);
+        let style = {top: `${top}vh`, left: `${left}vh`};
+        console.table({style, hyp, num_element, each_angle, top});
+
         return (
-            <li key={key}>
+            <li
+              key={key}
+              style={style}>
               <div className="description">
                 <img src={myImg} alt="skill"/>
                 <span className="key">{key}</span>
