910005d8b0c811bae43c6580df4cdbd2a31c7e09
otamam818 <otamam818@gmail.com>
2023-07-14 23:22:02 +1000

    Added new Skills section

---
+++ src/Skills/index.jsx
@@ -0,0 +1,13 @@
+import "./style.scss";
+
+function Skills() {
+  return (
+    <div className="my-skills">
+      <h1> Skills </h1>
+      
+    </div>
+  )
+}
+
+export default Skills;
+
--- src/index.js
+++ src/index.js
@@ -3,12 +3,14 @@ import ReactDOM from 'react-dom/client';
 import './styles/index.scss';
 import WelcomeContent from './WelcomeContent';
 import InfoCards from './InfoCards';
+import Skills from './Skills';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <WelcomeContent />
     <InfoCards />
+    <Skills />
   </React.StrictMode>
 );
 
