37446f123af541b9c601d528b77d76c7584cdaaf
otamam818 <otamam818@gmail.com>
2023-07-15 13:02:34 +1000

    Increased the density of polkadots, replaced magic numbers with constants, polished UI

--- src/WelcomeContent/WelcomeSplash.jsx
+++ src/WelcomeContent/WelcomeSplash.jsx
@@ -1,7 +1,8 @@
 import { useState, useEffect } from 'react';
 
-const NUM_ROWS = 4;
-const NUM_COLUMNS = 6;
+const NUM_ROWS = 8;
+const NUM_COLUMNS = 10;
+const DELAY_SCALER = 4;
 
 function WelcomeSplash () {
   const [dots, setDots] = useState(null)
@@ -11,7 +12,7 @@ function WelcomeSplash () {
     for (let i = 0; i < NUM_ROWS; i++) {
       let innerList = [];
       for (let j = 0; j < NUM_COLUMNS; j++) {
-        let delay = Math.ceil(Math.random() * 4);
+        let delay = Math.ceil(Math.random() * DELAY_SCALER);
         innerList.push(<div className='dot' style={
           {"--delay" : `${delay}s`}
         }>
