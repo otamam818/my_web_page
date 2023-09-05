020541925af8d57049309954d2340b1e02be1170
Tahmin Ahmed <otamam818@gmail.com>
2023-09-05 11:32:23 +1000

    Added new picture and logo, polished UI

--- src/WelcomeContent/index.tsx
+++ src/WelcomeContent/index.tsx
@@ -1,27 +1,15 @@
-import {useEffect, useRef, useState, CSSProperties} from "react";
+import {useRef} from "react";
 import "./style.scss";
 import WelcomeMessage from "./WelcomeMessage";
 import WelcomeSplash from "./WelcomeSplash";
-import mePic from "./me.jpeg" ;
+import mePic from "./me.png" ;
 
 function WelcomeContent () {
   const ref = useRef(null);
-  const [currHeight, setCurrHeight] = useState<null | number>(null);
-  useEffect(() => {
-    let chosenHeight;
-    if (ref.current) {
-      const refHeight = (ref.current as HTMLElement).offsetHeight;
-      chosenHeight = ((Math.ceil(refHeight / 100) + 2) * 100) - 50
-    } else {
-      chosenHeight = null;
-    }
-    setCurrHeight(chosenHeight);
-  }, [])
   return (
     <div
       className="welcome-content"
       ref={ref}
-      style={(currHeight && {height: currHeight}) as CSSProperties}
     >
       <WelcomeMessage />
       <img src={mePic} alt="me in the park" />
--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,9 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    host: true
+  },
   plugins: [react()],
   resolve: {
     alias: [{ find: '@', replacement: '/src' }]
